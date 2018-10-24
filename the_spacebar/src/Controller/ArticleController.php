ecfe6141dfcccfeddeab98c6a532fb84a0b752b9
Cornea Tudor-Sebastian <tudor.cornea@freshbyteinc.com>
2018-10-24 16:44:42 +0300

    Video9-10: Assets and Urls

--- the_spacebar/src/Controller/ArticleController.php
+++ the_spacebar/src/Controller/ArticleController.php
@@ -1,43 +1,51 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tudor
- * Date: 10/24/2018
- * Time: 11:03 AM
- */
 
 namespace App\Controller;
 
-
+use Psr\Log\LoggerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
-// de ce faci extindere la Abstract , deobicei nu e nevoie o faci pt shortcut methods
 class ArticleController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/", name="app_homepage")
      */
     public function homepage()
     {
-        return new Response('Omg my first page');
+        return $this->render('article/homepage.html.twig');
     }
 
     /**
-     * @Route("/news/{slug}")
+     * @Route("/news/{slug}", name="article_show")
      */
     public function show($slug)
     {
         $comments = [
-            'I ate a normal rock once. It did not taste like bacon!',
-            'I like bacon too! Buy some from my site!',
+            'I ate a normal rock once. It did NOT taste like bacon!',
+            'Woohoo! I\'m going on an all-asteroid diet!',
+            'I like bacon too! Buy some from my site! bakinsomebacon.com',
         ];
 
-
         return $this->render('article/show.html.twig', [
-            'title' => ucwords(str_replace('-', '', $slug)),
-            'comments' =>$comments,
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'slug' => $slug,
+            'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
+     */
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
+    {
+        // TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
+
+        return new JsonResponse(['hearts' => rand(5, 100)]);
     }
 }
