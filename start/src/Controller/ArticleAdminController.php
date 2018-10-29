7464a3a656eff37e0217ac0821a76f254162cc10
Cornea Tudor-Sebastian <tudor.cornea@freshbyteinc.com>
2018-10-29 16:09:37 +0200

    Course3Video12: Entity New fields+Update html.twig

--- start/src/Controller/ArticleAdminController.php
+++ start/src/Controller/ArticleAdminController.php
@@ -49,6 +49,10 @@ EOF
             $article->setPublishedAt(new \DateTime(sprintf('-%d days',rand(1, 100))));
         }
 
+        $article->setAuthor('Mike Ferengi')
+            ->setHeartCount(rand(5, 100))
+            ->setImageFilename('asteroid.jpeg');
+
         $em->persist($article);  //persist doar spune , as dori sa salvez acest article(obiect)
         $em->flush(); //doctrine nu face insertul , asa ca se executa flush ca sa se faca salvarea.
                       // de ce se face asta? poti sa ai 10 persisturi si doar un flush tada
