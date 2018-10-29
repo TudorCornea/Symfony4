d322ad700c5a864e151be0a7f2cecf5655814a30
Cornea Tudor-Sebastian <tudor.cornea@freshbyteinc.com>
2018-10-29 14:04:22 +0200

    Course3Video9: Entity Repo+changing the homepage

--- start/src/Controller/ArticleController.php
+++ start/src/Controller/ArticleController.php
@@ -29,9 +29,15 @@ class ArticleController extends AbstractController
     /**
      * @Route("/", name="app_homepage")
      */
-    public function homepage()
+    public function homepage(EntityManagerInterface $em)
     {
-        return $this->render('article/homepage.html.twig');
+        $repository = $em->getRepository(Article::class);
+        dump($repository);die;
+        $articles = $repository->findBy([], ['publishedAt' => 'DESC']); //daca nu pui nimic la primul array , o sa-ti dea tot
+
+        return $this->render('article/homepage.html.twig',[
+          'articles' => $articles
+        ]);
     }
 
     /**
