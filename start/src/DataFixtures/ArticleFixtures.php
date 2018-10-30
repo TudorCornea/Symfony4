748f4d709aa1963cf45b5617d2b5d76cae4c1305
Cornea Tudor-Sebastian <tudor.cornea@freshbyteinc.com>
2018-10-30 13:47:56 +0200

    Course4Video6: Fixture References& Relating Objects

--- start/src/DataFixtures/ArticleFixtures.php
+++ start/src/DataFixtures/ArticleFixtures.php
@@ -3,7 +3,6 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
-use App\Entity\Comment;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class ArticleFixtures extends BaseFixture
@@ -54,17 +53,7 @@ EOF
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
 
-            $comment1 = new Comment();
-            $comment1->setAuthorName('Mike Ferengi');
-            $comment1->setContent('I ate a normal rock once. It did NOT taste like bacon!');
-            $comment1->setArticle($article);
-            $manager->persist($comment1);
 
-            $comment2 = new Comment();
-            $comment2->setAuthorName('Mike Ferengi');
-            $comment2->setContent('Woohoo! I\'m going on an all-asteroid diet!');
-            $comment2->setArticle($article);
-            $manager->persist($comment2);
     });//persist doar spune , as dori sa salvez acest article(obiect)
         $manager->flush(); //doctrine nu face insertul , asa ca se executa flush ca sa se faca salvarea.
         // de ce se face asta? poti sa ai 10 persisturi si doar un flush tada
