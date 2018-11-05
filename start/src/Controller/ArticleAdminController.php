a827502959352863a9b0b40d84ca378f93a82759
Cornea Tudor-Sebastian <tudor.cornea@freshbyteinc.com>
2018-11-05 14:59:32 +0200

    Course6Vid2: Handling the Form Submit

--- start/src/Controller/ArticleAdminController.php
+++ start/src/Controller/ArticleAdminController.php
@@ -10,9 +10,11 @@ namespace App\Controller;
 
 use App\Entity\Article;
 use App\Form\ArticleFormType;
+use App\Repository\ArticleRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -23,10 +25,25 @@ class ArticleAdminController extends AbstractController
      * @Route("/admin/article/new", name="admin_article_new")
      *@IsGranted("ROLE_ADMIN_ARTICLE")
      */
-    public function new(EntityManagerInterface $em)
+    public function new(EntityManagerInterface $em, Request $request)
     {
         $form = $this->createForm(ArticleFormType::class);
 
+        $form->handleRequest($request); //by default handleRequest only processes the data only at post request
+                                        //daca e post , handlerul va lua datele de la submit automat
+                                        // iar jos la isSubmitted va returna true
+        if($form->isSubmitted() && $form->isValid()){
+            $data = $form->getData();
+            $article = new Article();
+            $article->setTitle($data['title']);
+            $article->setContent($data['content']);
+            $article->setAuthor($this->getUser());
+
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('app_homepage');
+        }
         return $this->render('article_admin/new.html.twig',[
             'articleForm' => $form->createView(), //createView transform the object into other object care e super bun la render
                                        //deci noi transformam obiectul form in alt obiect de tipul createView care e bun la render.
@@ -44,4 +61,15 @@ class ArticleAdminController extends AbstractController
         dd($article);
     }
 
+    /**
+     * @Route("/admin/article")
+     */
+    public function list(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findAll();
+
+        return $this->render('article_admin/list.html.twig',[
+            'articles' => $articles
+        ]);
+    }
 }
